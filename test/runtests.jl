618c8112618aee250f13e7ec731e7756193244aa
linus-md <linus.sommer@protonmail.com>
2024-06-19 21:32:09 +0200

    install AS from github

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,6 @@
 using Test
+using Pkg
+Pkg.add(url="https://github.com/algebraic-solving/AlgebraicSolving.jl")
 
 @testset verbose = true "DifferentialBases Tests" begin
     include("algorithms/classical.jl")

223c96db37a7c2b34840617396852658f83648ae
linus-md <linus.sommer@protonmail.com>
2024-06-19 23:26:37 +0200

    update

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,5 @@
 using Test
 using Pkg
-Pkg.add(PackageSpec(url="https://github.com/linus-md/AlgebraicSolving.jl/", rev="block"))
 
 @testset verbose = true "DifferentialBases Tests" begin
     include("algorithms/classical.jl")

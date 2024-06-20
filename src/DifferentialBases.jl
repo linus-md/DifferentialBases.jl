0227d89a917f3d5bb26442406f58053cff5748c3
Linus Sommer <95619282+linus-md@users.noreply.github.com>
2024-06-20 13:48:06 +0200

    add chemical systems and pendulums (#7)

--- src/DifferentialBases.jl
+++ src/DifferentialBases.jl
@@ -2,4 +2,5 @@ module DifferentialBases
 
 include("algorithms/classical.jl")
 include("systems/mechanical.jl")
+include("systems/chemical.jl")
 end
--- test/systems/mechanical.jl
+++ test/systems/mechanical.jl
@@ -1,9 +1,18 @@
-@testset "Systems -> mechanical -> differential_basis" begin
-    using DifferentialBases: simple_pendulum, masspoint_parabola
+@testset "Systems -> mechanical" begin
+    using DifferentialBases: 
+        simple_pendulum, double_pendulum, triple_pendulum, masspoint_parabola
 
     ideal, derivatives, R = simple_pendulum()
     @test length(ideal.gens) == 1
     @test length(derivatives) == 5
+
+    ideal, derivatives, R = double_pendulum()
+    @test length(ideal.gens) == 2
+    @test length(derivatives) == 10
+
+    ideal, derivatives, R = triple_pendulum()
+    @test length(ideal.gens) == 3
+    @test length(derivatives) == 15
 
     ideal, derivatives, R = masspoint_parabola()
     @test length(ideal.gens) == 1

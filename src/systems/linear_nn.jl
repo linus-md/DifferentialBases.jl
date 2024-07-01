d124b9cf67a23ed925f15c5710a1c475a0f26a8a
linus-md <linus.sommer@protonmail.com>
2024-07-01 14:30:48 +0200

    add tests

--- src/systems/linear_nn.jl
+++ src/systems/linear_nn.jl
@@ -7,23 +7,23 @@ function linear_nn_2(m, n, r)
         GF(101),
         :A=>(1:m, 1:n),
         :B=>(1:n, 1:r),
-        :x => (1:n),
-        :y => (1:n))
+        :x => (1:r),
+        :y => (1:m))
 
-    nvars = m*n + n*r + 2*n
+    nvars = m*n + n*r + r + m
 
     f = A*B*x - y
     f = f[1]^2 + f[2]^2
     
     derivatives = Dict(
-        i => derivative(f, i) for i in 1:nvars - 2*n + 1)
-    for i in nvars - 2*n + 1:nvars
+        i => derivative(f, i) for i in 1:nvars - r - m + 1)
+    for i in nvars - r - m + 1:nvars
         derivatives[i] = R(0)
     end
     
     #constraints = collect(Iterators.flatten(A*A' - B'*B))
     ideal = Ideal([R(0)])
-    return ideal, derivatives
+    return ideal, derivatives, R
 end
 
 function linear_nn_3(m, n, r, s)
@@ -32,24 +32,23 @@ function linear_nn_3(m, n, r, s)
         :A=>(1:m, 1:n), 
         :B=>(1:n, 1:r),
         :C=>(1:r, 1:s),
-        :x => (1:n), 
-        :y => (1:n))
+        :x => (1:s), 
+        :y => (1:m))
 
-    nvars = m*n + n*r + r*s + 2*n
+    nvars = m*n + n*r + r*s + s+m
 
     f = A*B*C*x - y
     f = f[1]^2 + f[2]^2
     
     derivatives = Dict(
-        i => derivative(f, i) for i in 1:nvars - 2*n + 1)
-    for i in nvars - 2*n + 1:nvars
+        i => derivative(f, i) for i in 1:nvars - s - m + 1)
+    for i in nvars - s - m + 1:nvars
         derivatives[i] = R(0)
     end
     
     #constraints = collect(Iterators.flatten(A*A' - B'*B))
     #push!(constraints, collect(Iterators.flatten(B*B' - C'*C)))
-    ideal = Ideal(R(0))
-    return ideal, derivatives
+    ideal = Ideal([R(0)])
+    return ideal, derivatives, R
 end
 
-f = linear_nn_2(2, 2, 2)
